805d213a26eb9e95205cbf4e149f180278eb68d3
Colton Reiher <92594818+CottonRey@users.noreply.github.com>
2023-02-15 22:15:58 -0800

    First Four Projects

---
+++ hello_cin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    String strMessage;
+    cout << "Please type a single word message" << endl;
+    cin >> strMessage;
+    cout << strMessage << endl;
+    return 0;
+}
