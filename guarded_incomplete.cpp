4dfadfbcbf14f84908b09b9630fb5eefef0c7e9b
Colton Reiher <92594818+CottonRey@users.noreply.github.com>
2023-05-10 21:03:59 -0700

    Exceptions Exercise

---
+++ guarded_incomplete.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdexcept>
+
+class Device {
+public:
+    Device();
+    virtual ~Device();
+    void enable();
+    void activate();
+
+private:
+    bool m_bEnabled;
+};
+
+Device::Device() : m_bEnabled(false) { } // Default Constructor
+Device::~Device() { }                    // Default Destructor
+
+void Device::enable()
+{
+    m_bEnabled = true;
+}
+
+void Device::activate()
+{
+    try{
+      if (!m_bEnabled){
+        throw std::runtime_error("Device must be enabled first!");
+      }
+      std::cout << "Device is now activated!" << std::endl;
+    } catch(std::runtime_error ex){
+      std::cout << ex.what() << std::endl;
+    }
+    return;
+}
+
+int main(int argc, const char * argv[]) {
+    Device device;
+    device.activate();
+    return 0;
+}
